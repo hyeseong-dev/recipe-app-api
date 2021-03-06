f4915e132118654b47d2368bc4ac01ba32d1a21a
hyeseong-lee <hyeseong43@gmail.com>
2021-03-06 22:22:41 +0900

    Added create tag feature.

---
+++ app/recipe/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from recipe import views
+
+app_name = 'recipe'
+
+#mapping the user to corresponding viewset
+router = DefaultRouter()
+#reverse url is 'recipe:tag-list'
+router.register('tags', views.TagViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
---
+++ app/recipe/views.py
@@ -0,0 +1,32 @@
+from rest_framework import viewsets, mixins
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+from core.models import Tag
+from recipe import serializers
+
+
+class TagViewSet(viewsets.GenericViewSet,
+                 mixins.ListModelMixin,
+                 mixins.CreateModelMixin):
+    """manage tags in the database"""
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    queryset = Tag.objects.all()
+    serializer_class = serializers.TagSerializer
+
+    # This method should be overriden
+    # if we dont want to modify query set based on current instance attributes
+    def get_queryset(self):
+        """RETURN OBJECTS FOR THE CURRENT AUTHENTICATED USER ONLY"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    # override this method for CreateModelMixin
+    # create operation is done here (unlike in UserModelSerializer)
+    # because serializer does not have user
+    # we pass user to serializer and save it
+    def perform_create(self, serializer):
+        """CREATE A NEW TAG"""
+        serializer.save(user=self.request.user)
+
